dc891712772248c371bb748002722c4ba70ea6b8
Scott-TechStar <johnniekips@gmail.com>
2023-08-11 04:00:27 -0700

    Updated the Console class Instances and the test files

---
+++ tests/test_models/test_save_reload_user.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+import os
+import sys
+
+# Get the absolute path to the directory containing this script
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
+# Calculate the absolute path to the project's root directory (two levels up)
+project_root = os.path.abspath(os.path.join(current_dir, "..", ".."))
+
+# Add the project root directory to the Python path
+sys.path.append(project_root)
+from models import storage
+from models.base_model import BaseModel
+from models.user import User
+
+all_objs = storage.all()
+print("-- Reloaded objects --")
+for obj_id in all_objs.keys():
+    obj = all_objs[obj_id]
+    print(obj)
+
+print("-- Create a new User --")
+my_user = User()
+my_user.first_name = "Betty"
+my_user.last_name = "Bar"
+my_user.email = "airbnb@mail.com"
+my_user.password = "root"
+my_user.save()
+print(my_user)
+
+print("-- Create a new User 2 --")
+my_user2 = User()
+my_user2.first_name = "John"
+my_user2.email = "airbnb2@mail.com"
+my_user2.password = "root"
+my_user2.save()
+print(my_user2)
