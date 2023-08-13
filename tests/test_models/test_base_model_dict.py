21078dffcf59b5ee22112e75e040786800d96dec
John Mwadime <johnniekips@gmail.com>
2023-08-13 11:00:33 +0300

    Update test_base_model_dict.py

--- tests/test_models/test_base_model_dict.py
+++ tests/test_models/test_base_model_dict.py
@@ -2,7 +2,7 @@
 import os
 import sys
 current_dir = os.path.dirname(os.path.abspath(__file__))
-project_root = os.path.abspath(os.path.join(current_dir, ".."))
+project_root = os.path.abspath(os.path.join(current_dir, "..",".."))
 sys.path.append(project_root)
 from models.base_model import BaseModel
 
