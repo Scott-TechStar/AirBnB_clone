64bbe487a95aeedf6e38ef7c78fa68cb5d8e10cd
John Mwadime <johnniekips@gmail.com>
2023-08-13 11:05:20 +0300

    Update test_file_storage.py

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -2,6 +2,11 @@
 """
 Test for storage
 """
+import os
+import sys
+current_dir = os.path.dirname(os.path.abspath(__file__))
+project_root = os.path.abspath(os.path.join(current_dir, "..","..","..")
+sys.path.append(project_root)
 from datetime import datetime
 import unittest
 from time import sleep
