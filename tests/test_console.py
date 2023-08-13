3cba756480d45c1decd4710fa4b26e9c82e05120
John Mwadime <johnniekips@gmail.com>
2023-08-13 10:35:38 +0300

    Update test_console.py

--- tests/test_console.py
+++ tests/test_console.py
@@ -13,6 +13,9 @@ Unittest classes:
 """
 import os
 import sys
+current_dir = os.path.dirname(os.path.abspath(__file__))
+project_root = os.path.abspath(os.path.join(current_dir, ".."))
+sys.path.append(project_root)
 import unittest
 from models import storage
 from models.engine.file_storage import FileStorage
