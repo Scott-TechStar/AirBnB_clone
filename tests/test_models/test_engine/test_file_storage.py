9de9401e0654750332ae5324f74653e312aed4ce
Scott-TechStar <johnniekips@gmail.com>
2023-08-13 01:17:10 -0700

    tests

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -4,11 +4,12 @@ Test for storage
 """
 import os
 import sys
-current_dir = os.path.dirname(os.path.abspath(__file__))
-project_root = os.path.abspath(os.path.join(current_dir, "..","..","..")
-sys.path.append(project_root)
-from datetime import datetime
 import unittest
+current_dir = os.path.dirname(os.path.abspath(__file__))
+project_root = os.path.abspath(os.path.join(current_dir, "..","..",".."))
+sys.path.append(project_root)
+                               
+from datetime import datetime
 from time import sleep
 import json
 from models.engine.file_storage import FileStorage
